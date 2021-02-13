045f26dd7aa7071d69a28a3babf572da6f296dbf
Vighnesh Raut <vighnesh.raut13@gmail.com>
2021-02-13 09:08:39 +0530

    chop off shit from meta

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -2,5 +2,6 @@ export const transformMeta = (meta) => {
   const result = {...meta};
   result.time = new Date(meta.time).toLocaleString();
   delete result.service;
+  delete result.requestId;
   return result;
 };
